52249ebb6c342247f33ae2671b3653a8c1672c56
Thorunitha <s-s.thorunitha@alstomgroup.com>
2022-01-27 11:45:42 +0530

    Add project files.

---
+++ BankOfDotNet.ConsoleClient/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using IdentityModel.Client;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace BankOfDotNet.ConsoleClient
+{
+    class Program
+    {
+
+        //Code to Consume IdentityServer
+
+        public static void Main(string[] args) => MainAsync().GetAwaiter().GetResult();
+        private static async Task MainAsync()
+        {
+
+            var discoRO = await DiscoveryClient.GetAsync("http://localhost:5000");
+            if (discoRO.IsError)
+            {
+                Console.WriteLine(discoRO.Error);
+                return;
+            }
+            //Grab a bearer token using Resource Owner
+            var tokenClientRO = new TokenClient(discoRO.TokenEndpoint, "ro.client", "secret");
+            var tokenResponseRO = await tokenClientRO.RequestResourceOwnerPasswordAsync("Thorunitha", "password", "BankOfDotNetApi");
+            if (tokenResponseRO.IsError)
+            {
+                Console.WriteLine(tokenResponseRO.Error);
+                return;
+            }
+            Console.WriteLine(tokenResponseRO.Json);
+            Console.WriteLine("\n\n");
+
+
+
+
+            //Discover all the endpoints using metadata of identity server
+            var disco = await DiscoveryClient.GetAsync("http://localhost:5000");
+            if (disco.IsError)
+            {
+                Console.WriteLine(disco.Error);
+                return;
+            }
+            //connect token and request token
+            var tokenClient = new TokenClient(disco.TokenEndpoint,"client","secret");
+            var tokenResponse = await tokenClient.RequestClientCredentialsAsync("BankOfDotNetApi");
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+            Console.WriteLine(tokenResponse.Json);
+            Console.WriteLine("/n/n");
+
+            //consume our customer API
+            var client = new HttpClient();
+            //Set Client Authorization header
+            client.SetBearerToken(tokenResponse.AccessToken);
+            var customerInfo = new StringContent(
+                JsonConvert.SerializeObject(
+                    new { Id = 10, FirstName = "sathiya", LastName = "Narayanan" }
+                    ), Encoding.UTF8, "application/json");
+            var createCustomerResponse = await client.PostAsync("http://localhost:52260/api/customers"
+                , customerInfo);
+            if (!createCustomerResponse.IsSuccessStatusCode)
+            {
+                Console.WriteLine(createCustomerResponse.StatusCode);
+            }
+            var getCustomerResponse=await client.GetAsync("http://localhost:52260/api/customers");
+            if (!getCustomerResponse.IsSuccessStatusCode)
+            {
+                Console.WriteLine(getCustomerResponse.StatusCode);
+            }
+            else
+            {
+                var content = await getCustomerResponse.Content.ReadAsStringAsync();
+                Console.WriteLine(JArray.Parse(content));
+                Console.ReadLine();
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ BankOfDotNet.IdentitySvr/Config.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityServer4;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+
+namespace BankDotNet.IdentitySvr
+{
+    public class Config
+    {
+        /*public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile()
+            };
+        }*/
+
+        public static List<TestUser> GetUsers()
+        {
+            return new List<TestUser>
+            {
+                new TestUser
+                {
+                    SubjectId="1",
+                    Username="Thorunitha",
+                    Password="password"
+                },
+                new TestUser
+                {
+                    SubjectId="2",
+                    Username="Thoru",
+                    Password="password"
+                }
+            };
+        }
+        //Set up resource and client
+        public static IEnumerable<ApiResource> GetAllApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("BankOfDotNetApi","Customer Api for BankofDotNet")
+            };
+        }
+        public static IEnumerable<Client> GetClients()
+        {
+            
+            return new List<Client>
+            {
+                //Client-Credential based grant type
+                new Client
+                {
+                    ClientId="client",
+                    AllowedGrantTypes=GrantTypes.ClientCredentials,
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes={ "BankOfDotNetApi" }
+                },
+                  //Resource Owner Password Grant type
+                new Client
+                {
+                    ClientId="ro.client",
+                    AllowedGrantTypes=GrantTypes.ResourceOwnerPassword,
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes={"BankOfDotNetApi"}
+                }
+                //Implicit Flow Grant Type
+              /*  new Client
+                {
+                    ClientId="mvc",
+                    ClientName="MVC Client",
+                    AllowedGrantTypes=GrantTypes.Implicit,
+                    //User is redirected from the client to Identity server4
+                    RedirectUris={"http://localhost:5003/signin-oidc" },
+                    PostLogoutRedirectUris={"http://localhost:5003/signout-callback-oidc"},
+                    AllowedScopes=new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile
+                    }
+                }*/
+            };
+        }
+    }
+}
